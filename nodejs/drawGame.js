1632fe1d3b078872832373967692f11f72642981
DSka <dska@DSkas-MBP-2.home>
2019-01-02 21:25:11 -0500

    added conditional statement that end game when infection deck reaches 2 cards, created connection lines between cities on the map

--- nodejs/drawGame.js
+++ nodejs/drawGame.js
@@ -1,8 +1,8 @@
 
-let infectionDeck = ['Bangkok','San Francisco', 'Bangkok', 'Bangkok', 'Hong Kong', 'Paris', 'New York'];
+let infectionDeck = ['Bangkok', 'Bangkok', 'Bangkok', 'San Francisco','Hong Kong', 'Paris', 'New York'];
 let usedInfDeck = []
 
-
+//creates visiable depth to the card deck
 for(var i=0; i < 5; i++){
 	var newDiv = document.createElement('div');
 	   newDiv.id = 'r';
@@ -38,7 +38,7 @@ function showShuffleDeck(deck){
 			x.innerHTML = deck.join(", ")
 }
 
-//draws card from infection deck
+//picks up a card from infection deck
 function drawINFcard(drawDeck, usedDeck){
 	usedDeck.unshift(drawDeck.shift())
 	let x = document.getElementById("card")
@@ -47,10 +47,15 @@ function drawINFcard(drawDeck, usedDeck){
 	state.numOfCards = infectionDeck.length
 	var parent = document.getElementsByClassName("cardSpace")
 	var child = document.getElementById("r");
-	parent[0].removeChild(child)
-	//infect city
+	// game ends when Infection deck has 2 cards left
+	if(infectionDeck.length <= 2){
+		console.log("Game Over")
+		return
+	} else if(infectionDeck.length <= 5){
+			parent[0].removeChild(child)
+		}
+	//infects city based on picked up card
 	infect(usedDeck[0])
-
 }
 
 
@@ -71,8 +76,11 @@ function drawMap(ctx, image){
       }, 50);
       return;
     }
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    drawCity()
+	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+	//put city and connecting lines on the map after map image is louaded
+	drawConnection()
+	drawCity()
+	
 }
 
     // Create a new image object
@@ -87,41 +95,73 @@ function drawMap(ctx, image){
 let cityCord = [[654,336,'Bangkok','red'],[45, 249, 'San Francisco', 'cyan'],
 [381,209, 'Paris', 'cyan'], [695, 305,'Hong Kong','red'], [180,243,'New York','cyan']]
 
+//connection hubs, first city in each nested array connect to all other cities in that array
+let connect = [['Paris', 'Bangkok', 'Hong Kong']]
+
+function drawConnection(){
+let flatCC = cityCord.flat()
+let cityIdx,y1,x1, y2,x2
+connect.forEach(function(connectCity){
+	cityIdx = flatCC.indexOf(connectCity[0])
+	x1 = flatCC[cityIdx-1]
+	y1 = flatCC[cityIdx-2]
+	console.log(x1, y1)
+	for(let i = 1; i < connectCity.length; i ++){
+		cityIdx = flatCC.indexOf(connectCity[i])
+		x2 = flatCC[cityIdx-1]
+		y2 = flatCC[cityIdx-2]
+		ctx.beginPath();
+		ctx.lineWidth = "5";
+		ctx.moveTo(y1, x1);
+		ctx.lineTo(y2, x2);
+		ctx.stroke();
+	}		
+	})
+}
+
+
 function drawCity(){
 	cityCord.forEach(function(city){
-
+		ctx.save();
 		ctx.fillStyle = city[3] 
 		ctx.beginPath();
 		ctx.arc(city[0], city[1], 8, 0, Math.PI * 2);
 		ctx.fill();
-			
+		ctx.restore();	
 		}
 	)
 }
 
-//infects city by 1 native disease. 
+//infects city by with native disease. 
 
 function infect(newINFcity){
-	if (isOutbreak(newINFcity) === 1){
-		console.log('hi')
-		offsetX = 5, offsetY = 5
-		
+	let setX = 0 
+	let setY = 0
+	let diseaseCount = isOutbreak(newINFcity)
+	if (diseaseCount === 1){
+		setX = - 5 , setY = 10	
 	}
-	cityCord.forEach(function(city, offsetX, offsetY){
+	if (diseaseCount === 2){
+		setX = - 5 , setY = -10	
+	}
+	cityCord.forEach(function(city){
+
 		let cityName = city[2]
 		let diseaseColor = city[3]
-		let x = offsetX + city[1]
-			if(newINFcity === cityName){
+		let x = setX + city[1]
+		let y = setY + city[0]
+	
+			if(newINFcity === cityName){	
 				ctx.fillStyle = diseaseColor
 				ctx.strokeStyle = 'white'
 				ctx.beginPath();
-				ctx.arc(city[0], x +12, 7, 0, Math.PI * 2);
+				ctx.arc(y, x +12, 5, 0, Math.PI * 2);
 				ctx.fill()
 				ctx.stroke();
 				//updates GS with infected city and disease color
 				state.city[cityName].push(diseaseColor)
+				//checks for outbreak
 				isOutbreak(cityName)
-				console.log(state.city)
 				return 
 			}	
 		})
